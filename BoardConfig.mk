707d8a5d2329d1e5feb3b4e3eb213c50ce1a7ba7
Woomymy <woomy@woomy.be>
2023-02-27 18:37:44 +0100

    X6882: Add recovery init scripts and required flags
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I6a3b4ba0037d9fec49e8445ed8e615b2d14e8c7f
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,5 +102,11 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
 
+# Recovery
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6789
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USERIMAGES_USE_F2FS := true
+
 # Inherit the proprietary files
 include vendor/infinix/X6882/BoardConfigVendor.mk

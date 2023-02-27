7f8dfe4096e9245de0c654209dfca2a1e7460285
Woomymy <woomy@woomy.be>
2023-02-27 15:33:13 +0100

    X6882: Inherit virtual ab launch_with_vendor_ramdisk makefile
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: Ie8f796d45b3d303cd784dcc702769d17e075b250
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -3,7 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Shipping Api level
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces

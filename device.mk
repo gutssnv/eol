681ab3cedd819a6fc880c8cd622e016133a1a88e
Sarthak Roy <sarthakroy2002@gmail.com>
2025-06-26 20:03:17 +0900

    X6882: Switch to libperfmgr from hardware/google
    * Also drop no-op imports
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I22dcf417b7feb2379ed77761bd851317e0c8a955
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -368,7 +368,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr
+    android.hardware.power-service.pixel-libperfmgr
 
 PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub:64 \
@@ -464,7 +464,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
-    hardware/lineage/interfaces/power-libperfmgr \
     hardware/google/interfaces \
     hardware/google/pixel
 

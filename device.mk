05c75e143286da45ff9b8828aa6e36fb6d340e09
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2025-07-26 23:16:40 +0900

    X6882: Build AIDL Light HAL
    * Also set ro.vendor.transsion.backlight_12bit=1 so it wouldn't flicker on max brightness
    * Using our new Rust-based implementation
    
    Change-Id: Iac04ed7447e6e13d82dbdebe80103e975beec1b5
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -161,6 +161,10 @@ PRODUCT_PACKAGES += \
     TetheringConfigNoir \
     WifiOverlayNoir
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.lineage
+
 # Keymaster
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml

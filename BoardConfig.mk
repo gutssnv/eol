c1efe24c2884a05c89534856af70777767a8a0d6
gutssnv <decryptor69@gmail.com>
2026-08-15 04:03:47 +0700

    X6882: Initial template properties
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,10 @@
 
 DEVICE_PATH := device/infinix/X6882
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
+
 # Inherit the proprietary files
 include vendor/infinix/X6882/BoardConfigVendor.mk

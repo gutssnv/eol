b1e58a9b81d095c3edcdc0d0468213a7e685663b
gutssnv <decryptor69@gmail.com>
2026-08-16 02:14:23 +0700

    X6882: link older libbinder & libhidlbase to camera provider
    Change-Id: I36782e50423b423a7e4c5c924e35d8c46faa3d63
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -68,6 +68,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libbinder.so', 'libbinder-v31.so')
         .replace_needed('libutils.so', 'libutils-v31.so'),
     'vendor/lib64/hw/mt6789/android.hardware.camera.provider@2.6-impl-mediatek.so': blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v31.so')
+        .replace_needed('libbinder.so', 'libbinder-v31.so')
         .replace_needed('libutils.so', 'libutils-v31.so')
         .add_needed('libcamera_metadata_shim.so'),
 }  # fmt: skip

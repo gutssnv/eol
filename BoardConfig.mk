javas-prjkt/device_infinix_X6882

X6882: Set BOOT_SECURITY_PATCH
Change-Id: I8ae16175a4f471098326e920bfdf42a7fa316bff

BoardConfig.mk
@@ -183,7 +183,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 TARGET_OTA_ASSERT_DEVICE := X6882,Infinix-X6882
 
 # Vendor Security Patch
-BOOT_SECURITY_PATCH := $(VENDOR_SECURITY_PATCH)
+BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Verified Boot

gutssnv/eol

Signed-off-by: gutssnv <decryptor69@gmail.com>

BoardConfig.mk
@@ -178,6 +178,7 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor Security Patch
+BOOT_SECURITY_PATCH := $(VENDOR_SECURITY_PATCH)
 VENDOR_SECURITY_PATCH := 2025-11-05
 
 # Verified Boot